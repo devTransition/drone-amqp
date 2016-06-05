66035390a1706d1b33db665df25219fcdcb08b4d
Valiantsin Luchko <akmeful@gmail.com>
2016-06-05 19:02:37 +0300

    publish message to amqp server

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# drone-rabbitmq
+# drone-amqp
+
+## install vendor packages
+
+Use https://github.com/govend/govend
+
+Get dependencies running: 
+```sh
+govend -v --prune
+```
+
+## build
+
+```sh
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GO15VENDOREXPERIMENT=1 go build -ldflags '-s -w' -o drone-amqp
+```
+
+```sh
+docker build --rm -t local/drone-amqp .
+```
+
+## Run as drone plugin
+
+Use PLUGIN_FILTER env variable to allow running of `local/drone-amqp` docker image
+```
+PLUGIN_FILTER="plugins/* local/*"
+```
+
+
+## testing
+```sh
+GO15VENDOREXPERIMENT=1 go run main.go types.go amqp.go < test.json
+```
+
+or
+
+```sh
+GO15VENDOREXPERIMENT=1 go run main.go types.go amqp.go <<EOF
+{
+  "repo": {
+    "clone_url": "git://github.com/drone/drone",
+    "owner": "drone",
+    "name": "drone",
+    "full_name": "drone/drone"
+  },
+  "system": {
+    "link_url": "https://beta.drone.io"
+  },
+  "build": {
+    "number": 22,
+    "status": "success",
+    "started_at": 1421029603,
+    "finished_at": 1421029813,
+    "message": "Update the Readme",
+    "author": "johnsmith",
+    "author_email": "john.smith@gmail.com",
+    "event": "push",
+    "branch": "master",
+    "commit": "436b7a6e2abaddfd35740527353e78a227ddcb2c",
+    "ref": "refs/heads/master"
+  },
+  "workspace": {
+    "root": "/drone/src",
+    "path": "/drone/src/github.com/drone/drone"
+  },
+  "vargs": {
+    "Connection": {
+      "Host": "192.168.99.100",
+      "Username": "guest",
+      "Password": "guest"
+    },
+    "Exchange": "some-exchange",
+    "Key": "some-key",
+    "Mandatory": true,
+    "Publishing": {
+      "Headers": {
+        "header0": "header-zero",
+        "header1": "header-one"
+      },
+      "ContentType": "application/x-json",
+      "ContentEncoding": "",
+      "DeliveryMode": 1,
+      "Priority": 5,
+      "CorrelationId": "Some-CorrelationId",
+      "ReplyTo": "RPC",
+      "Expiration": "Expiration-spec",
+      "MessageId": "Some-MessageId",
+      "Type": "Some-Type",
+      "UserId": "Some-UserId",
+      "AppId": "Some-AppId"
+    },
+    "Template": "{\"git_branch\": \"{{ build.branch }}\"}"
+  }
+}
+EOF
+```
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+  //"crypto/tls"
+  //"encoding/json"
+  "fmt"
+  //"io/ioutil"
+  //"net/http"
+  //"net/url"
+  "github.com/drone/drone-go/drone"
+  "github.com/drone/drone-go/plugin"
+  "github.com/drone/drone-go/template"
+  //"github.com/coreos/coreos-kubernetes/multi-node/aws/_gopath-vendor/src/golang.org/x/tools/cmd/vet/testdata/divergent"
+  "bytes"
+  "os"
+  "github.com/streadway/amqp"
+  "log"
+)
+
+const (
+  respFormat = "Webhook %d\n  URL: %s\n  RESPONSE STATUS: %s\n  RESPONSE BODY: %s\n"
+  debugRespFormat = "Webhook %d\n  URL: %s\n  METHOD: %s\n  HEADERS: %s\n  REQUEST BODY: %s\n  RESPONSE STATUS: %s\n  RESPONSE BODY: %s\n"
+)
+
+var (
+  buildCommit string
+)
+
+func main() {
+  fmt.Printf("Drone RabbitMQ Plugin built from %s\n", buildCommit)
+
+  system := drone.System{}
+  repo := drone.Repo{}
+  build := drone.Build{}
+  vargs := Params{}
+
+  plugin.Param("system", &system)
+  plugin.Param("repo", &repo)
+  plugin.Param("build", &build)
+  plugin.Param("vargs", &vargs)
+  plugin.MustParse()
+
+  // Creates the payload, by default the payload
+  // is the build details in json format, but a custom
+  // template may also be used.
+  
+  var buf bytes.Buffer
+  err := template.Write(&buf, vargs.Template, &drone.Payload{
+    Build:  &build,
+    Repo:   &repo,
+    System: &system,
+  })
+  
+  if err != nil {
+    log.Printf("Error: Failed to execute the content template. %s\n", err)
+    os.Exit(1)
+  }
+  
+  log.Printf("Data: %q, Template: %s, Payload: %s", vargs, vargs.Template, buf.String());
+  msg := amqp.Publishing{
+    Headers: vargs.Publishing.Headers,
+    // Properties
+    ContentType:     vargs.Publishing.ContentType,    // MIME content type
+    ContentEncoding: vargs.Publishing.ContentEncoding,    // MIME content encoding
+    DeliveryMode:    vargs.Publishing.DeliveryMode,     // Transient (0 or 1) or Persistent (2)
+    Priority:        vargs.Publishing.Priority,     // 0 to 9
+    CorrelationId:   vargs.Publishing.CorrelationId,    // correlation identifier
+    ReplyTo:         vargs.Publishing.ReplyTo,    // address to to reply to (ex: RPC)
+    Expiration:      vargs.Publishing.Expiration,    // message expiration spec
+    MessageId:       vargs.Publishing.MessageId,    // message identifier
+    Type:            vargs.Publishing.Type,    // message type name
+    UserId:          vargs.Publishing.UserId,    // creating user id - ex: "guest"
+    AppId:           vargs.Publishing.AppId,    // creating application id
+
+    // The application specific payload of the message
+    Body: buf.Bytes(),
+  }
+  
+  err = PublishMesssage(
+    vargs.Connection.Host,
+    vargs.Connection.Username,
+    vargs.Connection.Password,
+    vargs.Exchange,
+    vargs.Key,
+    vargs.Mandatory,
+    vargs.WaitConfirm,
+    &msg,
+  )
+  
+  if err != nil {
+    log.Printf("Error: PublishMesssage %s\n", err)
+    os.Exit(1)
+  }
+  
+}
