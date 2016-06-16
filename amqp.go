89b650f34e3c61f9dbe1df2db91e9ca49866bdbe
Valiantsin Luchko <akmeful@gmail.com>
2016-06-16 18:07:46 +0300

    gofmt

--- amqp.go
+++ amqp.go
@@ -1,91 +1,91 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "github.com/streadway/amqp"
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
 )
 
 type AmqpConnection struct {
-  conn    *amqp.Connection
+	conn *amqp.Connection
 }
 
 func createAmqpConnection(user string, password string, host string) (*AmqpConnection, error) {
-  
-  uri := "amqp://" + user + ":" + password+ "@" + host 
-  c := &AmqpConnection{conn: nil}
-  
-  var err error
-  
-  log.Printf("Dialing to %q", uri)
-  c.conn, err = amqp.Dial(uri)
-  
-  if err != nil {
-    return nil, fmt.Errorf("Dial error to %q: %s", uri, err)
-  }
-  
-  go func() {
-    fmt.Printf("closing: %s", <-c.conn.NotifyClose(make(chan *amqp.Error)))
-  }()
-  
-  return c, nil
+
+	uri := "amqp://" + user + ":" + password + "@" + host
+	c := &AmqpConnection{conn: nil}
+
+	var err error
+
+	log.Printf("Dialing to %q", uri)
+	c.conn, err = amqp.Dial(uri)
+
+	if err != nil {
+		return nil, fmt.Errorf("Dial error to %q: %s", uri, err)
+	}
+
+	go func() {
+		fmt.Printf("closing: %s", <-c.conn.NotifyClose(make(chan *amqp.Error)))
+	}()
+
+	return c, nil
 
 }
 
 func (c *AmqpConnection) Shutdown() error {
 
-  defer log.Printf("AMQP Connection shutdown OK")
+	defer log.Printf("AMQP Connection shutdown OK")
 
-  if err := c.conn.Close(); err != nil {
-    return fmt.Errorf("AMQP connection close error: %s", err)
-  }
+	if err := c.conn.Close(); err != nil {
+		return fmt.Errorf("AMQP connection close error: %s", err)
+	}
 
-  return nil
+	return nil
 
 }
 
-func PublishMesssage (host string, user string, password string, exchange string, key string, mandatory bool, waitConfirm bool, msg *amqp.Publishing) (error) {
-  
-  amqpConnection, err := createAmqpConnection(user, password, host)
-  
-  if err != nil {
-    return err
-  }
-  
-  channel, err := amqpConnection.conn.Channel()
-  
-  if err != nil {
-    return fmt.Errorf("Channel error: %s", err)
-  }
-  
-  var confirms <-chan amqp.Confirmation
-  
-  if waitConfirm {
-    confirms = channel.NotifyPublish(make(chan amqp.Confirmation, 1))
-  }
-  
-  err = channel.Publish(exchange, key, mandatory, false, *msg)
-  
-  if err != nil {
-    return fmt.Errorf("Publish error: %s", err)
-  }
-  
-  var confirmed amqp.Confirmation
-  
-  if waitConfirm {
-    if confirmed = <-confirms; confirmed.Ack {
-      fmt.Printf("Published, exchange: %s, key: %s, message: %q", exchange, key, msg)
-    } else {
-      //log.Printf("Not published to AMQP, %q, %d", delivery.CorrelationId, confirmed.DeliveryTag)
-      return fmt.Errorf("Not published, exchange: %s, key: %s, message: %q", exchange, key, msg)
-    }
-  }
-  
-  err = amqpConnection.Shutdown()
-  
-  if err != nil {
-    return err
-  }
-  
-  return nil
+func PublishMesssage(host string, user string, password string, exchange string, key string, mandatory bool, waitConfirm bool, msg *amqp.Publishing) error {
+
+	amqpConnection, err := createAmqpConnection(user, password, host)
+
+	if err != nil {
+		return err
+	}
+
+	channel, err := amqpConnection.conn.Channel()
+
+	if err != nil {
+		return fmt.Errorf("Channel error: %s", err)
+	}
+
+	var confirms <-chan amqp.Confirmation
+
+	if waitConfirm {
+		confirms = channel.NotifyPublish(make(chan amqp.Confirmation, 1))
+	}
+
+	err = channel.Publish(exchange, key, mandatory, false, *msg)
+
+	if err != nil {
+		return fmt.Errorf("Publish error: %s", err)
+	}
+
+	var confirmed amqp.Confirmation
+
+	if waitConfirm {
+		if confirmed = <-confirms; confirmed.Ack {
+			fmt.Printf("Published, exchange: %s, key: %s, message: %q", exchange, key, msg)
+		} else {
+			//log.Printf("Not published to AMQP, %q, %d", delivery.CorrelationId, confirmed.DeliveryTag)
+			return fmt.Errorf("Not published, exchange: %s, key: %s, message: %q", exchange, key, msg)
+		}
+	}
+
+	err = amqpConnection.Shutdown()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- types.go
+++ types.go
@@ -1,40 +1,41 @@
 package main
+
 import "github.com/streadway/amqp"
 
 // Params represents the valid parameter options for the rabbitmq plugin.
 type Params struct {
-  Connection Connection
-  Exchange string
-  Key string
-  Mandatory bool
-  //Immediate bool
-  WaitConfirm bool
-  Publishing Publishing
-  Template   string
+	Connection Connection
+	Exchange   string
+	Key        string
+	Mandatory  bool
+	//Immediate bool
+	WaitConfirm bool
+	Publishing  Publishing
+	Template    string
 }
 
 // Connection represents amqp connection options.
 type Connection struct {
-  Host     string
-  Username string
-  Password string
+	Host     string
+	Username string
+	Password string
 }
 
 type Publishing struct {
-                         // Application or exchange specific fields,
-                         // the headers exchange will inspect this field.
-  Headers         amqp.Table
-  
-                         // Properties
-  ContentType     string // MIME content type
-  ContentEncoding string // MIME content encoding
-  DeliveryMode    uint8  // Transient (0 or 1) or Persistent (2)
-  Priority        uint8  // 0 to 9
-  CorrelationId   string // correlation identifier
-  ReplyTo         string // address to to reply to (ex: RPC)
-  Expiration      string // message expiration spec
-  MessageId       string // message identifier
-  Type            string // message type name
-  UserId          string // creating user id - ex: "guest"
-  AppId           string // creating application id
+	// Application or exchange specific fields,
+	// the headers exchange will inspect this field.
+	Headers amqp.Table
+
+	// Properties
+	ContentType     string // MIME content type
+	ContentEncoding string // MIME content encoding
+	DeliveryMode    uint8  // Transient (0 or 1) or Persistent (2)
+	Priority        uint8  // 0 to 9
+	CorrelationId   string // correlation identifier
+	ReplyTo         string // address to to reply to (ex: RPC)
+	Expiration      string // message expiration spec
+	MessageId       string // message identifier
+	Type            string // message type name
+	UserId          string // creating user id - ex: "guest"
+	AppId           string // creating application id
 }
